dfbd4d4a89ed9e2f56de69b54e495f3b1277371b
mcubillos <cubillos.maria0@gmail.com>
2015-09-11 15:42:22 -0500

    Including new necessary files

---
+++ app/js/controllers/checkBox.js
@@ -0,0 +1,246 @@
+formApp.controller('checkBoxController', function ($scope, $http) {
+ 
+      // toggle selection for a given employee by name
+    $scope.toggleSelection = function toggleSelection(selection, name) {
+
+        var idx = selection.indexOf(name);
+        // is currently selected
+        if (idx > -1) {
+            selection.splice(idx, 1);
+        }
+        // is newly selected
+        else {
+            selection.push(name);
+        }
+    };
+
+    $scope.updateSelectionFeeding = function updateSelectionEvent(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.annualCycleAtomizedType.Event = element.name;
+    };
+
+   $scope.updateSelectionFeeding = function updateSelectionFeeding(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.feedingAtomizedType.type = element.name;
+    };
+
+    $scope.updateSelectionThropic = function updateSelectionThropic(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.thropic.strategy = element.name;
+    };
+
+    $scope.updateSelectionDispersalType = function updateSelectionDispersalType(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.formData.dispersal.dispersalAtomized.type = element.name;
+    };
+
+    $scope.updateSelectionStructureDispersed = function updateSelectionStructureDispersed(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.formData.dispersal.dispersalAtomized.structureDispersed = element.name;
+    };
+
+    $scope.updateSelectionOrigin = function updateSelectionOrigin(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.invasivenessAtomizedType.origin = element.name;
+    };
+
+    $scope.updateSelectionPresence = function updateSelectionPresence(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.invasivenessAtomizedType.presence = element.name;
+    };
+
+    $scope.updateSelectionPersistence = function updateSelectionPersistence(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.invasivenessAtomizedType.persistence = element.name;
+    };
+
+    $scope.updateSelectionDistribution = function updateSelectionDistribution(name) {
+        var idx = $scope.invasivenessAtomizedType.distribution.indexOf(name);
+        // is currently selected
+        if (idx > -1) {
+            $scope.invasivenessAtomizedType.distribution.splice(idx, 1);
+        }
+        // is newly selected
+        else {
+            $scope.invasivenessAtomizedType.distribution.push(name);
+        }
+    };
+
+    $scope.updateSelectionHarmful = function updateSelectionDistribution(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.invasivenessAtomizedType.harmful = element.name;
+    };
+
+    $scope.updateSelectionAbundance = function updateSelectionAbundance(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.invasivenessAtomizedType.abundance = element.name;
+    };
+    $scope.updateSelectionTrend = function updateSelectionTrend(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.invasivenessAtomizedType.trend = element.name;
+    };
+    $scope.updateSelectionRateOfSpread = function updateSelectionRateOfSpread(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.invasivenessAtomizedType.rateOfSpread = element.name;
+    };
+
+    $scope.updateSelectionRegulatoryListing = function updateSelectionRegulatoryListing(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.invasivenessAtomizedType.regulatoryListing = element.name;
+    };
+
+    $scope.updateSelectionLocalityType = function updateSelectionLocalityType(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.invasivenessAtomizedType.localityType = element.name;
+    };
+
+    $scope.updateSelectionLocationValue = function updateSelectionLocationValue(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.invasivenessAtomizedType.locationValue = element.name;
+    };
+    $scope.updateSelectionPublicationDatePrecision = function updateSelectionPublicationDatePrecision(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.invasivenessAtomizedType.publicationDatePrecision = element.name;
+    };
+
+    $scope.updateSelectionDistributionScope = function updateSelectionDistributionScope(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.distributionClass.distributionScope.type = element.name;
+    };
+
+    $scope.updateSelectionRegion = function updateSelectionRegion(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.populationBiologyAtomized.region = element.name;
+    };
+
+    $scope.updateSelectionThreatCategory = function updateSelectionThreatCategory(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.threatStatusClass.threatStatusAtomized.threatCategory = element.name;
+    };
+
+    $scope.updateSelectionAutority = function updateSelectionAutority(element) {
+         var idx = $scope.threatStatusClass.threatStatusAtomized.authority.indexOf(name);
+        if (idx > -1) {
+            $scope.threatStatusClass.threatStatusAtomized.authority.splice(idx, 1);
+        }
+        else {
+            $scope.threatStatusClass.threatStatusAtomized.authority.push(name);
+        }
+    };
+    $scope.updateSelectionProtectionLegal = function updateSelectionProtectionLegal(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.legislationAtomizedType.protectionLegalStatus = element.name;
+    };
+    $scope.updateSelectionProtectionLegalStatus = function updateSelectionProtectionLegalStatus(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.legislationAtomizedType.status = element.name;
+    };
+    $scope.updateSelectionProtectionLegalType = function updateSelectionProtectionLegalType(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.legislationAtomizedType.type = element.name;
+    };
+    $scope.updateSelectionProtectionLegalNorm = function updateSelectionProtectionLegalNorm(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.legislationAtomizedType.norm = element.name;
+    };
+     $scope.updateSelectionPartUsedCategory = function updateSelectionPartUsedCategory(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.usesAtomizedType.partUsed = element.name;
+    };
+     $scope.updateSelectionUseTypeAtomized = function updateSelectionUseTypeAtomized(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.usesAtomizedType.useTypeAtomized = element.name;
+    };
+     $scope.updateSelectionManagementType = function updateSelectionManagementType(element, list) {
+        angular.forEach(list, function(item) {
+            item.checked = false;
+        });
+        element.checked= true;
+        $scope.managementAndConservationAtomizedType.type = element.name;
+    };
+    $scope.updateSelectionManagementAction = function updateSelectionManagementAction(element) {
+         var idx = $scope.managementAndConservationAtomizedType.actions.indexOf(name);
+        if (idx > -1) {
+            $scope.managementAndConservationAtomizedType.actions.splice(idx, 1);
+        }
+        else {
+            $scope.managementAndConservationAtomizedType.actions.push(name);
+        }
+    };
+    
+});
---
+++ app/js/controllers/hierarchy.js
@@ -0,0 +1,10 @@
+h2 Jerarquía
+button(type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal") Editar
+div(class="modal-content")
+	div(class="modal-header")
+		button(type="button" class="close" data-dismiss="modal") &times;
+		h4(class="modal-tittle") Editar Jerarquía
+	div(class="modal-body")
+
+	div(class="modal-footer")
+		button(type="button" class="btn btn-default" data-dismiss="modal") Close
