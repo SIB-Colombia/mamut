ef87c472af48d54149a41ab4fd0228504fddcbe8
mcubillos <cubillos.maria0@gmail.com>
2015-11-30 11:23:16 -0500

    Some text changes

--- src/app/resources/javascripts/form/form.ctrl.js
+++ src/app/resources/javascripts/form/form.ctrl.js
@@ -369,4 +369,10 @@ angular.module('app.controllers.form',[])
 		
 		$scope.selectedAttr = [];
 		$scope.date = new Date();	
-}]);
+}])
+.filter('split', function() {
+	return function(input, splitChar, splitIndex) {
+	// do some bounds checking here to ensure it has that index
+	return input.split(splitChar)[splitIndex];
+	}
+});
